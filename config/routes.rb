46b79e6ce6ee4b265197df0506aa32b4622ab5d2
nullmann1 <nick.ullmann1@gmail.com>
2022-03-09 23:18:47 +0000

    Commit message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  root "projects#index"
   resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
